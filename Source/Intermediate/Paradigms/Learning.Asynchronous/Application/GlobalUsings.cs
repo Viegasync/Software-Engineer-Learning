8ffe8abda49dc0f7ffc88d337eb0eca0d0d3e521
Brendo Pereira Viegas <159083802+Viegasync@users.noreply.github.com>
2025-09-15 15:44:30 -0300

    chore: add global usings for project dependencies

---
+++ Source/Intermediate/Paradigms/Learning.Asynchronous/Application/GlobalUsings.cs
@@ -0,0 +1,16 @@
+﻿global using System;
+global using System.Collections.Concurrent;
+global using System.Collections.Generic;
+global using System.Diagnostics;
+global using System.Linq;
+global using System.Net.Http;
+global using System.Net.Http.Json;
+global using System.Reflection;
+global using System.Text;
+global using System.Text.Json.Serialization;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using Learning.Asynchronous.Application;
+global using Learning.Asynchronous.Extensions;
+global using Learning.Asynchronous.Models;
+global using Learning.Asynchronous.Services;
