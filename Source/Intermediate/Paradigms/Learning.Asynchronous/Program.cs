1cd36b5e438f2700c3b44527e57a75a96b4f9101
Brendo Pereira Viegas <159083802+Viegasync@users.noreply.github.com>
2025-09-15 15:46:12 -0300

    feat: add Program entry point

--- Source/Intermediate/Paradigms/Learning.Asynchronous/Program.cs
+++ Source/Intermediate/Paradigms/Learning.Asynchronous/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿await ZipCodeProcessor.RunAsync(["01001-000", "90010-000", "S305-071H", "00000-000", " "]);
