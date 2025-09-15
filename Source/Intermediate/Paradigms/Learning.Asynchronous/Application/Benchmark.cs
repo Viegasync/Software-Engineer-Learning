7258079814857aab247274c4505c644764af643a
Brendo Pereira Viegas <159083802+Viegasync@users.noreply.github.com>
2025-09-15 15:44:02 -0300

    feat: add Benchmark class to measure execution time

---
+++ Source/Intermediate/Paradigms/Learning.Asynchronous/Application/Benchmark.cs
@@ -0,0 +1,53 @@
+﻿namespace Learning.Asynchronous.Application;
+
+/// <summary>
+/// Classe estática para medir o tempo de execução 
+/// de operações, baseada em <see cref="Stopwatch"/>.
+/// </summary>
+internal static class Benchmark
+{
+    /// <summary>
+    /// Tempo decorrido.
+    /// </summary>
+    private static Stopwatch s_stopwatch;
+
+    /// <summary>
+    /// Inicia o cronômetro interno.
+    /// </summary>
+    internal static void Start() =>
+        s_stopwatch = Stopwatch.StartNew();
+
+    /// <summary>
+    /// Para o cronômetro interno.
+    /// </summary>
+    /// <exception cref="ArgumentException"></exception>
+    internal static void Stop()
+    {
+        ThrowIfNull();
+        s_stopwatch.Stop();
+    }
+
+    /// <summary>
+    /// Exibe o tempo decorrido em segundos 
+    /// desde que o cronômetro foi iniciado.
+    /// </summary>
+    /// <exception cref="ArgumentException"></exception>
+    internal static void DisplayElapsedTime()
+    {
+        ThrowIfNull();
+        Console.Write($"\nTime: {s_stopwatch.Elapsed.TotalSeconds:F2}");
+        Console.ReadKey();
+    }
+
+    /// <summary>
+    /// Ocorre uma exceção 
+    /// se <see cref="Stopwatch"/>
+    /// não tiver sido inicializado.
+    /// </summary>
+    /// <exception cref="ArgumentException"></exception>
+    private static void ThrowIfNull()
+    {
+        if (s_stopwatch is null)
+            throw new ArgumentException("The benchmark has not been started.");
+    }
+}
