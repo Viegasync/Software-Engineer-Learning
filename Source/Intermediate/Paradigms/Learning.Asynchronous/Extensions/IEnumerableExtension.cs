c958cd69b162b95b5dd1a3b92bb603c01787385b
Brendo Pereira Viegas <159083802+Viegasync@users.noreply.github.com>
2025-09-15 15:45:58 -0300

    feat: extension method for IEnumerable

---
+++ Source/Intermediate/Paradigms/Learning.Asynchronous/Extensions/IEnumerableExtension.cs
@@ -0,0 +1,18 @@
+﻿namespace Learning.Asynchronous.Extensions;
+
+/// <summary>
+/// Classe estática que contém métodos de 
+/// extensão para <see cref="IEnumerable{T}"/>.
+/// </summary>
+internal static class IEnumerableExtension
+{
+    /// <summary>
+    /// Executa uma ação para 
+    /// cada elemento da sequência.
+    /// </summary>
+    internal static void ForEach<TSource>(this IEnumerable<TSource> source, Action<TSource> action)
+    {
+        if (source is null || action is null) return;
+        foreach (TSource item in source) action(item);
+    }
+}
